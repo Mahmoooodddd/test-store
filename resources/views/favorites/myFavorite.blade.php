4cab09dd6ba98973577777a6c9e3975eaa0b812a
mahmoooodddd <mahmoudmohsenifar@gmail.com>
2020-04-11 00:13:32 +0430

    change the favorite table

--- resources/views/favorites/myFavorite.blade.php
+++ resources/views/favorites/myFavorite.blade.php
@@ -1,34 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach($favoriteProducts as  $favoriteProduct)
-        <table>
+    {{--@foreach($favoriteProducts as  $favoriteProduct)--}}
+        <table class="table table-bordered table-hover">
             <thead>
             <tr>
                 <th>id</th>
                 <th>name</th>
             </tr>
             </thead>
+            <tbody>
             <tr>
+
+                @foreach($favoriteProducts as  $favoriteProduct)
                 <td>{{$favoriteProduct->id}}</td>
                 <td>{{$favoriteProduct->name}}</td>
 
             </tr>
-        </table>
-        <div>
-            <button type="button" class="show-favorite" favoriteProduct-id="{{$favoriteProduct->id}}">Show favorites
-            </button>
-            <button type="button" class="unLike-product" favoriteProduct-id="{{$favoriteProduct->id}}">unLike</button>
+        {{--</table>--}}
+        {{--<div>--}}
+            {{--<button type="button" class="show-favorite" favoriteProduct-id="{{$favoriteProduct->id}}">Show favorites--}}
+            {{--</button>--}}
+            {{--<button type="button" class="unLike-product" favoriteProduct-id="{{$favoriteProduct->id}}">unLike</button>--}}
             {{--<p>{{$favoriteProduct->id}}</p>--}}
             {{--<p>{{$favoriteProduct->name}}</p>--}}
             {{--<button type="button" class="show-favorite" favoriteProduct-id="{{$favoriteProduct->id}}">Show favorites--}}
             {{--</button>--}}
             {{--<button type="button" class="unLike-product" favoriteProduct-id="{{$favoriteProduct->id}}">unLike</button>--}}
-        </div>
-        <hr>
+        {{--</div>--}}
+        {{--<hr>--}}
 
 
     @endforeach
+            </tbody>
+    </table>
 
 
 @endsection
@@ -39,7 +44,7 @@
     <script type="text/javascript">
         $(document).ready(function () {
 
-            myFavorite.init();
+            // myFavorite.init();
         });
     </script>
 
